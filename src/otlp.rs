fb21a3bc7f6b7f4f6faa6839101eaad4142f2e74
Allex Veldman <allexveldman@gmail.com>
2024-07-16 16:07:53 +0200

    feat: Add trace and span ID to logs
    Use the root span ID as the TraceID and the current span ID as the
    SpanID.

--- src/otlp.rs
+++ src/otlp.rs
@@ -6,8 +6,8 @@ use prost::Message;
 use time::OffsetDateTime;
 use tracing::Subscriber;
 use tracing_core::Event;
-use tracing_subscriber::{layer::Context, Layer};
-use worker::{console_debug, console_error, console_log, Cf};
+use tracing_subscriber::{layer::Context, registry::LookupSpan, Layer};
+use worker::{console_log, Cf};
 
 use tracing::field::{Field, Visit};
 
@@ -19,7 +19,8 @@ use opentelemetry_proto::tonic::resource::v1::Resource;
 
 use crate::USER_AGENT;
 
-/// Convert a batch of log records into a byte array and content type
+/// Convert a batch of log records into a ExportLogsServiceRequest
+/// <https://opentelemetry.io/docs/specs/otlp/#otlpgrpc>
 fn build_logs_export_body(logs: Vec<LogRecord>, cf: &Cf) -> ExportLogsServiceRequest {
     let scope_logs = ScopeLogs {
         scope: None,
@@ -90,6 +91,7 @@ pub async fn flush(
     otlp_auth: String,
     cf: &Cf,
 ) {
+    console_log!("Flushing logs to OTLP");
     let client = reqwest::Client::builder()
         .user_agent(USER_AGENT)
         .build()
@@ -100,7 +102,10 @@ pub async fn flush(
         let log_records = match receiver.recv().await {
             Ok(request) => request,
             // Channel is empty and closed, so we're done here
-            Err(_) => break,
+            Err(_) => {
+                console_log!("Channel is empty and closed");
+                break;
+            }
         };
         let body = build_logs_export_body(log_records, cf).encode_to_vec();
         let mut url = url::Url::parse(&otlp_endpoint).unwrap();
@@ -116,13 +121,14 @@ pub async fn flush(
         {
             Ok(response) => {
                 if !response.status().is_success() {
-                    console_error!("Failed to send logs to OTLP: {:?}", response);
+                    console_log!("Failed to send logs to OTLP: {:?}", response);
+                    console_log!("Response body: {:?}", response.text().await.unwrap());
                 } else {
-                    console_debug!("Logs sent to OTLP: {:?}", response);
+                    console_log!("Logs sent to OTLP: {:?}", response);
                 };
             }
             Err(err) => {
-                console_error!("Failed to send logs to OTLP: {:?}", err);
+                console_log!("Error sending logs to OTLP: {:?}", err);
             }
         };
         // If the channel is empty, we're done
@@ -143,14 +149,53 @@ impl OtlpLogLayer {
     }
 }
 
-impl<S: Subscriber> Layer<S> for OtlpLogLayer {
-    fn on_event(&self, event: &Event<'_>, _ctx: Context<'_, S>) {
+impl<S> Layer<S> for OtlpLogLayer
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    fn on_event(&self, event: &Event<'_>, ctx: Context<'_, S>) {
+        let Some(time_ns) = time_unix_ns() else {
+            return;
+        };
+
+        // Get the log level and message
         let level = event.metadata().level();
         let mut visitor = LogVisitor::default();
         event.record(&mut visitor);
 
-        let Some(time_ns) = time_unix_ns() else {
-            return;
+        // Extract the span and trace IDs
+        // we'll consider the current span ID as the trace ID when there is no parent span
+        let (trace_id, span_id) = match ctx.event_span(event) {
+            Some(mut span) => {
+                let mut span_id = span.id().into_u64().to_be_bytes().to_vec();
+                span_id.resize(8, 0);
+                let mut trace_id = loop {
+                    match span.parent() {
+                        Some(parent) => {
+                            span = parent;
+                        }
+                        None => {
+                            break span.id().into_u64().to_be_bytes().to_vec();
+                        }
+                    }
+                };
+                trace_id.resize(16, 0);
+                (Some(trace_id), Some(span_id))
+            }
+            None => {
+                let mut span_id = ctx
+                    .current_span()
+                    .id()
+                    .map(|id| id.into_u64().to_be_bytes().to_vec());
+                let mut trace_id = span_id.clone();
+                if let Some(id) = span_id.as_mut() {
+                    id.resize(8, 0)
+                }
+                if let Some(id) = trace_id.as_mut() {
+                    id.resize(16, 0)
+                }
+                (span_id, trace_id)
+            }
         };
 
         let log_record = LogRecord {
@@ -163,8 +208,8 @@ impl<S: Subscriber> Layer<S> for OtlpLogLayer {
             }),
             attributes: vec![],
             dropped_attributes_count: 0,
-            trace_id: vec![],
-            span_id: vec![],
+            trace_id: trace_id.unwrap_or_default(),
+            span_id: span_id.unwrap_or_default(),
             flags: 0,
         };
 
@@ -173,7 +218,7 @@ impl<S: Subscriber> Layer<S> for OtlpLogLayer {
 
     fn on_close(&self, _id: tracing_core::span::Id, _ctx: Context<'_, S>) {
         if let Err(err) = self.flush() {
-            console_error!("Failed to flush log records: {:?}", err);
+            console_log!("Failed to flush log records: {:?}", err);
         }
     }
 }
@@ -182,7 +227,7 @@ fn time_unix_ns() -> Option<u64> {
     match OffsetDateTime::now_utc().unix_timestamp_nanos().try_into() {
         Ok(value) => Some(value),
         Err(_) => {
-            console_error!("SystemTime out of range for conversion to u64!");
+            console_log!("SystemTime out of range for conversion to u64!");
             None
         }
     }
