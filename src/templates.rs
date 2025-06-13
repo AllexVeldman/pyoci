d063dfff0c39b4a81f98538644494e50381d8ce4
Allex Veldman <allexveldman@gmail.com>
2025-06-13 16:41:11 +0200

    refactor(templates): Replace askama with handlebars
    Swap the templating engine to handlebars.
    
    `Package.py_uri()` no longer includes the sha256 to prevent escapes of
    `=` during templating. Instead the sha256 is exposed when `Package` is
    serialized.

--- src/templates.rs
+++
@@ -1,9 +0,0 @@
-use crate::package::{Package, WithFileName};
-use askama::Template;
-
-#[derive(Template)]
-#[template(path = "list-package.html")]
-pub struct ListPackageTemplate<'a> {
-    pub subpath: &'a str,
-    pub files: Vec<Package<'a, WithFileName>>,
-}
